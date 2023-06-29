c5d91998299b99d81b22abb724defb741a4de2bc
JulWe <132477835+JulWe@users.noreply.github.com>
2023-06-29 11:02:35 +0200

    added the index.html, script.js & style.css

---
+++ source/script.js
@@ -0,0 +1,134 @@
+let currentTime = new Date();
+
+let appDate = document.querySelector(".date");
+
+let currentDate = currentTime.getDate();
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[currentTime.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[currentTime.getMonth()];
+
+appDate.innerHTML = `${day}, ${currentDate} ${month} ${hours}:${minutes}`;
+
+let city = "vienna";
+let units = "metric";
+let apiKey = "bc21775749a463dd976c3ae303c75079";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+function searchCity(event) {
+  event.preventDefault;
+  let searchInput = document.querySelector("#search-text-input");
+
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+    let units = "metric";
+    let apiKey = "bc21775749a463dd976c3ae303c75079";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showWeather);
+  } else {
+    h1.innerHTML = null;
+    alert("Please type a city");
+  }
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
+function showWeather(response) {
+  let city = response.data.name;
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = `${city}`;
+
+  let temperature = Math.round(response.data.main.temp);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${temperature} C°`;
+
+  let sky = response.data.weather[0].description;
+  let h3 = document.querySelector("h3");
+  h3.innerHTML = `${sky}`;
+
+  let maxTemperature = Math.round(response.data.main.temp_max);
+  let maxtemp = document.querySelector("#maxtemp");
+  maxtemp.innerHTML = `${maxTemperature}`;
+
+  let minTemperature = Math.round(response.data.main.temp_min);
+  let mintemp = document.querySelector("#mintemp");
+  mintemp.innerHTML = `${minTemperature} `;
+
+  let feelsLike = Math.round(response.data.main.feels_like);
+  let feel = document.querySelector("#feelslike");
+  feel.innerHTML = `${feelsLike} °C`;
+
+  let windSpeed = response.data.wind.speed;
+  let speed = document.querySelector("#windspeed");
+  speed.innerHTML = `${windSpeed} km/h`;
+
+  let airPressure = response.data.main.pressure;
+  let pressure = document.querySelector("#pressure");
+  pressure.innerHTML = `${airPressure} mb`;
+
+  let airHumidity = response.data.main.humidity;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${airHumidity} %`;
+}
+axios.get(apiUrl).then(showWeather);
+
+function getCurrentPosition(event) {
+  event.preventDefault;
+  navigator.geolocation.getCurrentPosition(currentCity);
+}
+
+function currentCity(event) {
+  event.preventDefault;
+  let apiKey = "bc21775749a463dd976c3ae303c75079";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+//function getPosition(position) {
+//let latitude = position.coords.latitude;
+//let longitude = position.coords.longitude;
+//let apiKey = "bc21775749a463dd976c3ae303c75079";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+//axios.get(apiUrl).then(showWeather);
+
+//navigator.geolocation.getCurrentPosition(currentCity);
+//}
+
+let button = document.querySelector(".current");
+button.addEventListener("click", getCurrentPosition);
+
+//function changeToCelsius(event) {
+//event.preventDefault();
+//let temperatureLink = document.querySelector("#temperature");
+//temperatureLink.innerHTML = 22;
+//}
+
+//let linkFarenheit = document.querySelector("#farenheit-link");
+//linkFarenheit.addEventListener("click", changeToFarenheit);
+
+//let linkCelsius = document.querySelector("#celsius-link");
+//linkCelsius.addEventListener("click", changeToCelsius);
